f8db7846282bd0bfb5c77ef9b1e0735f3f90344d
vbazhynov <vladbazhynov@gmail.com>
2022-07-11 22:44:12 +0300

    create rooms

--- public/javascript/game.mjs
+++ public/javascript/game.mjs
@@ -1,7 +1,109 @@
+import { showInputModal, showMessageModal } from './views/modal.mjs';
+import { appendRoomElement, removeRoomElement } from './views/room.mjs';
+import { appendUserElement } from './views/user.mjs';
+
 const username = sessionStorage.getItem('username');
 
 if (!username) {
-	window.location.replace('/login');
+  window.location.replace('/login');
 }
 
 const socket = io('', { query: { username } });
+
+const createRoomModal = () => {
+  const onChange = roomName => {
+    sessionStorage.setItem('room', roomName);
+  };
+  const onSubmit = () => {
+    const roomName = sessionStorage.getItem('room');
+    const gamePageEl = document.querySelector('#game-page');
+    gamePageEl.classList.remove('display-none');
+    const roomsPageEl = document.querySelector('#rooms-page');
+    roomsPageEl.classList.add('display-none');
+    const roomNameEl = document.querySelector('#room-name');
+    roomNameEl.innerHTML = roomName;
+    socket.emit('join_room', roomName, username);
+    appendUserElement({
+      username: sessionStorage.getItem('username'),
+      ready: false,
+      isCurrentUser: true,
+    });
+  };
+
+  showInputModal({
+    title: 'Enter Room Name',
+    onChange,
+    onSubmit,
+  });
+};
+
+const onBackToRooms = () => {
+  const gamePageEl = document.querySelector('#game-page');
+  gamePageEl.classList.add('display-none');
+  const roomsPageEl = document.querySelector('#rooms-page');
+  roomsPageEl.classList.remove('display-none');
+  const roomNameEl = document.querySelector('#room-name');
+  const roomName = roomNameEl.innerText;
+  console.log('room name ' + roomName);
+  socket.emit('leave_room', roomName);
+};
+
+const backToRoomsBtn = document.querySelector('#quit-room-btn');
+backToRoomsBtn.addEventListener('click', onBackToRooms);
+
+const createRoomBtn = document.querySelector('#add-room-btn');
+createRoomBtn.addEventListener('click', createRoomModal);
+
+const onUserExistError = () => {
+  const onClose = () => {
+    sessionStorage.removeItem('username');
+    console.log('Yes');
+    window.location.replace('/login');
+  };
+
+  showMessageModal({
+    message: 'User Already Exist!',
+    onClose,
+  });
+};
+
+const onJoinDone = userNames => {
+  const currentUser = sessionStorage.getItem('username');
+  const usersContainer = document.querySelector('#users-wrapper');
+  usersContainer.innerHTML = '';
+  for (let user of userNames) {
+    const isCurrentUser = user === currentUser ? true : false;
+    appendUserElement({
+      username: user,
+      ready: false,
+      isCurrentUser: isCurrentUser,
+    });
+  }
+};
+
+const onDeleteRoom = name => {
+  removeRoomElement(name);
+};
+
+socket.on('delete_room', onDeleteRoom);
+socket.on('join_done', onJoinDone);
+socket.on('user_exist', onUserExistError);
+socket.on('update_rooms', rooms => {
+  console.log(rooms);
+  const onJoin = e => {
+    const roomName = e.target.dataset.roomName;
+    const gamePageEl = document.querySelector('#game-page');
+    gamePageEl.classList.remove('display-none');
+    const roomsPageEl = document.querySelector('#rooms-page');
+    roomsPageEl.classList.add('display-none');
+    const roomNameEl = document.querySelector('#room-name');
+    roomNameEl.innerHTML = roomName;
+    socket.emit('join_room', roomName);
+  };
+  const roomsContainer = document.querySelector('#rooms-wrapper');
+  roomsContainer.innerHTML = '';
+  for (let key in rooms) {
+    const usersNumber = rooms[key].length;
+    appendRoomElement({ name: key, numberOfUsers: usersNumber, onJoin });
+  }
+});
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,8 +1,60 @@
+import { Socket } from 'dgram';
 import { Server } from 'socket.io';
 import * as config from './config';
+export const usersMap = {};
+const roomsObj = {};
 
 export default (io: Server) => {
-	io.on('connection', socket => {
-		const username = socket.handshake.query.username;
-	});
+  io.on('connection', socket => {
+    const username = socket.handshake.query.username;
+    if (Object.keys(usersMap).length) {
+      for (let value of Object.values(usersMap)) {
+        if (value === username) {
+          socket.emit('user_exist', username);
+          return;
+        }
+      }
+    }
+    console.log(`User: ${username} Connected to server`);
+    usersMap[socket.id] = username;
+
+    socket.emit('update_rooms', roomsObj);
+
+    socket.on('disconnect', () => {
+      console.log(`User: ${socket.id} Disonnected from server`);
+      delete usersMap[socket.id];
+      console.log(usersMap);
+    });
+
+    socket.on('leave_room', roomId => {
+      let usersArray: string[] = [];
+      console.log('leave');
+      socket.leave(roomId);
+      socket.broadcast.emit('update_rooms', roomsObj);
+      const rooms = io.of('/').adapter.rooms;
+      const roomUsers = rooms.get(roomId) as Set<string>;
+      if (roomUsers) {
+        usersArray = [...roomUsers.keys()];
+        roomsObj[roomId] = usersArray;
+        const mappedNames = usersArray.map(user => usersMap[user]);
+        socket.broadcast.emit('update_rooms', roomsObj);
+        io.to(roomId).emit('join_done', mappedNames);
+      } else {
+        delete roomsObj[roomId];
+        io.emit('delete_room', roomId);
+        console.log(roomsObj);
+      }
+    });
+
+    socket.on('join_room', roomId => {
+      socket.join(roomId);
+      const rooms = io.of('/').adapter.rooms;
+      const roomUsers = rooms.get(roomId) as Set<string>;
+      const usersArray = [...roomUsers.keys()];
+      roomsObj[roomId] = usersArray;
+      const mappedNames = usersArray.map(user => usersMap[user]);
+      socket.broadcast.emit('update_rooms', roomsObj);
+      io.to(roomId).emit('join_done', mappedNames);
+    });
+  });
 };
